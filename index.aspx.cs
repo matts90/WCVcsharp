dc33330d06bd73bf06c7c72f4c2c0321a29575cd
matts90 <seilermr@dukes.jmu.edu>
2017-03-27 22:40:05 -0400

    I suck

--- index.aspx.cs
+++ index.aspx.cs
@@ -9,6 +9,6 @@ public partial class index : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        // Matt Still Sucks
+        // Drew is the one that sucks... ij
     }
 }
